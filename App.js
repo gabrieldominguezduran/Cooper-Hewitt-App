91744b9ee4d4d8e51ab118c4e47810b6cd91a9c2
Gabriel Domínguez Durán <gabrieldominguezduran@gmail.com>
2020-02-29 16:05:58 +0100

    Json query made

--- App.js
+++ App.js
@@ -1,12 +1,79 @@
-import React from 'react'
-import {View, Text} from 'react-native'
+import React, { Component } from 'react'
+import {
+    StyleSheet,
+    View,
+    Text,
+    Image,
+    FlatList,
+  } from 'react-native'
 
-const App = () => {
-  return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text style ={{color: 'darkred', fontSize: 50}}>Hello world</Text>
+export default class Home extends Component {
+  constructor() {
+    super()
+    this.state = {
+      dataSource: []
+    }
+  }
+
+  renderItem = ({ item }) => {
+    return(
+      <View>
+        <Image
+        style={{width: 250, height: 250, margin: 10,}}
+        source={{ uri: item.images }}/>
+        <View>
+          <Text>
+            {item.title}
+          </Text>
+          <Text>
+            {item.description}
+          </Text>
+        </View>
     </View>
-  )
+    )
+    
+  }
+
+  componentDidMount() {
+    const url = 'https://api.collection.cooperhewitt.org/rest/?method=cooperhewitt.exhibitions.getObjects&access_token=d5b4aa8389d940aa8f0bcf72e89b9b84&tag=activist-poster&per_page=30'
+
+    fetch(url)
+    .then((response) => response.json())
+    .then((responseJson) => {
+
+      let values = []
+
+      responseJson.objects.map((data) => {
+        values.push({images: data.images[0].n.url, title: data.title, description: data.description})    
+      })
+      
+        this.setState({
+          dataSource: values
+        })
+    })
+    .catch((error) => {
+      console.log(error)
+      
+    })
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <FlatList
+          data={this.state.dataSource}
+          renderItem={this.renderItem}
+          keyExtractor={(item, index) => index}
+        />
+      </View>
+    )
+  }
 }
 
-export default App
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+})
